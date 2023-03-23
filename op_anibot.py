7db1b2c9feb485bbc3bdfd33ce7d3b7e963f6729
oprogramas <128657033+oprogramas@users.noreply.github.com>
2023-03-22 22:24:39 -0300

    Create op_anibot.py
    commit inicial

---
+++ op_anibot.py
@@ -0,0 +1,105 @@
+import glob
+import tweepy
+import time
+import random
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+
+
+api_key = ''
+api_secret_key = ''
+bearer_token = ''
+access_token = ''
+access_token_secret = ''
+
+auth = tweepy.OAuthHandler(api_key, api_secret_key)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+caminho = 'D:/anime_pics/'
+imagens = glob.glob(caminho + '/*.jpg')
+imagem = random.choice(imagens)
+
+
+def cria_meme(imagem, texto):
+	with Image.open(imagem).convert("RGBA") as base:
+
+		# cria uma imagem em branco para o texto e com transparencia total
+	    txt = Image.new("RGBA", base.size, (255, 255, 255, 0))
+
+	    # seleciona a fonte
+	    fnt = ImageFont.truetype("C:/Windows/Fonts/impact.ttf", 40)
+
+	    # cria o objeto desenho
+	    d = ImageDraw.Draw(txt)
+
+	    # justifica o texto
+	    wraped_text = textwrap.wrap(texto, width=35)
+	    text = '\n'.join(wraped_text)
+
+	    # verifica o tamanho do texto para centralização
+	    text_size = d.textsize(text, font=fnt)
+
+	    # define x e y para parametros de definição do local do texto
+	    x = (base.size[0] - text_size[0]) / 2
+	    y = (base.size[1] - text_size[1]) / 1.2
+
+	    # inclui o texto na imagem em branco
+	    d.multiline_text((x, y), text, font=fnt, fill=(255, 255, 255), stroke_width=5, stroke_fill=(199,21,133), align='center')
+
+	    # junta o texto com a imagem  
+	    out = Image.alpha_composite(base, txt)
+
+	    caminho_salvar = 'D:/Downloads/anime_pics/imagem_twt.png'
+
+	    out.convert("RGB").save(caminho_salvar)
+
+	    return caminho_salvar
+
+
+nm_arquivo = 'ultimo_id.txt'
+
+def recuperar_ultimo_id(nm_arquivo):
+	f_read = open(nm_arquivo, 'r')
+	ultimo_id = int(f_read.read().strip())
+	f_read.close()
+	return ultimo_id
+
+def guardar_ultimo_id(ultimo_id, nm_arquivo):
+	f_write = open(nm_arquivo, 'w')
+	f_write.write(str(ultimo_id))
+	f_write.close()
+	return
+
+
+def respondendo_tweets():
+	print('recuperando e respondendo tweets...', flush = True)
+
+	ultimo_id = recuperar_ultimo_id(nm_arquivo)
+
+	mentions = api.mentions_timeline(ultimo_id, tweet_mode = 'extended')
+
+	for mention in reversed(mentions):
+		print(str(mention.id) + ' - ' + mention.full_text, flush = True)
+		ultimo_id = mention.id
+		guardar_ultimo_id(ultimo_id, nm_arquivo)
+
+		print('menção encontrada', flush=True)
+		print('mandando meme...', flush=True)
+
+		texto = mention.full_text[14:-1]
+
+		status = '@' + mention.user.screen_name
+
+		meme = cria_meme(imagem, texto)
+
+		imagem_upload = api.media_upload(meme)
+
+		imagem_id = [imagem_upload.media_id_string]
+
+		api.update_status(in_reply_to_status_id = ultimo_id, media_ids = imagem_id, status = status)
+
+
+while True:
+	respondendo_tweets()
+	time.sleep(15)
